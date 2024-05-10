6544c64a38ab72333b76a51eb9656387f92873db
Meet Jain <meetj665@gmail.com>
2024-05-10 17:11:39 +0530

    canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -40,13 +40,22 @@ export default function Dashboard() {
         <span className="border flex-auto  max-w-min p-2 justify-end border-slate-300 flex">
             <span>Sort   </span>&nbsp;&nbsp;
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-  <path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V3a.75.75 0 0 1 .75-.75Zm-9 13.5a.75.75 0 0 1 .75.75v2.25a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V16.5a.75.75 0 0 1 1.5 0v2.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V16.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
+  <path fill-rule="evenodd" d="M6.97 2.47a.75.75 0 0 1 1.06 0l4.5 4.5a.75.75 0 0 1-1.06 1.06L8.25 4.81V16.5a.75.75 0 0 1-1.5 0V4.81L3.53 8.03a.75.75 0 0 1-1.06-1.06l4.5-4.5Zm9.53 4.28a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V7.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
 </svg>
 
+
             </span>
-        <span className="border flex-auto  max-w-min p-2 justify-end border-slate-300 ">ffff</span>
+        <span className="border flex-auto  max-w-min p-2 justify-end border-slate-300 ">
+            
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V3a.75.75 0 0 1 .75-.75Zm-9 13.5a.75.75 0 0 1 .75.75v2.25a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V16.5a.75.75 0 0 1 1.5 0v2.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V16.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
+</svg>
+        </span>
         </span>
     
+        </div>
+        <div className="mt-2">
+            <Table></Table>
         </div>
 
         </div>
@@ -57,3 +66,55 @@ export default function Dashboard() {
   )
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Table (){
+  
+        return (
+            <>
+            
+
+            <div className="flex space-x-10 bg-gray-300  justify-between ">
+
+                <div className="flex px-6 border   min-w-36 text-center  border-slate-400 " >jf           ff</div>
+                <div className="flex px-6 border   min-w-36 text-center  border-slate-400 " >jf           ff</div>
+                <div className="flex px-6 border   min-w-36 text-center  border-slate-400 " >jf           ff</div>
+                <div className="flex px-6 border   min-w-36 text-center  border-slate-400 " >jfff</div>
+
+
+            </div>
+                <br/>
+                <hr className="" />
+                <br/>
+
+
+
+
+            <div className="flex space-x-10 bg-gray-300  justify-between ">
+
+                <div className="flex px-6 border   min-w-36 text-center  border-slate-400 " >jf           ff</div>
+                <div className="flex px-6 border   min-w-36 text-center  border-slate-400 " >jf           ff</div>
+                <div className="flex px-6 border   min-w-36 text-center  border-slate-400 " >jf           ff</div>
+                <div className="flex px-6 border   min-w-36 text-center  border-slate-400 " >jfff</div>
+
+
+            </div>
+                <hr />
+                <hr />
+                <hr />
+            </>
+        )
+   
+   
+}
