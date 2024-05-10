6ad11ef21a07aacdd8ce3062324cf2b6bda870d6
Meet Jain <meetj665@gmail.com>
2024-05-10 13:16:00 +0530

    card component

---
+++ src/components/CardComponent.tsx
@@ -0,0 +1,60 @@
+// import { useState, useEffect, useMemo, useCallback, memo } from 'react'
+
+
+export default function CardComponent  (porps:any){
+
+
+
+
+
+
+    return (
+        <>
+        <div>
+
+        asdfasdf
+        </div>
+        <div className="  m-4  min-w-fit max-w-sm rounded-xl overflow-hidden shadow-lg border   bg-slate-300 border-slate-600"> 
+      
+            
+
+            <div className='   mx-8 my-4 flex-initial flex flex-row tracking-tight text-left font-bold text-gray-700'>
+                <h3>Amount Processed</h3>
+             
+             <p>Icon</p>
+                
+
+            </div>
+
+        
+                <div className="flex px-4 ">
+                    <div className=" flex-initial ">
+                        <h3>₹ 23,92,312.19</h3>
+                        
+                    </div>
+
+                    <div className=" flex flex-auto    justify-end ">
+                    {
+  props.link ? (
+    <>
+      <h3>adilf</h3>
+      <p>icon</p>
+    </>
+  ) : (
+    <>
+      <h3>adilf</h3>
+      <p>icon</p>
+    </>
+  )
+}
+
+                    </div>
+                </div>
+                <div className="flex flex-initial mt-8 border  border-blue-800 bg-blue-400">
+                    asdfasdf
+                </div>
+      
+        </div>
+        </>
+    )
+}
