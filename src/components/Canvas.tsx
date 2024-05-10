cf99d0bd134dd9284cf991509fdb3152da63b481
Meet Jain <meetj665@gmail.com>
2024-05-10 16:45:17 +0530

    canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -29,6 +29,29 @@ export default function Dashboard() {
      <div className="m-5 p-4">
         adfadsfadsf
      </div>
+
+
+     <div className="bg-white mx-10 p-4 box-border border border-slate-300 ">
+        <div className="flex justify-between space-x-6">
+
+        <span className="border p-2 flex-initial justify-start border-slate-300 ">ffff</span>
+        <span className="flex  justify-between space-x-6">
+
+        <span className="border flex-auto  max-w-min p-2 justify-end border-slate-300 flex">
+            <span>Sort   </span>&nbsp;&nbsp;
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V3a.75.75 0 0 1 .75-.75Zm-9 13.5a.75.75 0 0 1 .75.75v2.25a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V16.5a.75.75 0 0 1 1.5 0v2.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V16.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
+</svg>
+
+            </span>
+        <span className="border flex-auto  max-w-min p-2 justify-end border-slate-300 ">ffff</span>
+        </span>
+    
+        </div>
+
+        </div>
+       
+
       <CardComponent/>
     </div>
   )
